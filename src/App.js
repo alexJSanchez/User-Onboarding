e191ee49f13b0fd04f53388a700c9812e9ade943
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-31 22:19:44 -0400

    form works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
+import React, { useState } from 'react';
 import './App.css';
-import Form from './component/Form'
+import Form from './component/Form';
+
+const initialFormValues = {
+   username: '',
+   email: '',
+   password: '',
+   tos: false,
+}
+
 
 function App() {
+  const [ formValues ,  setFormValues] = useState(initialFormValues);
+  
+  const handleChange = (name, value) => {
+    setFormValues({...formValues, [name]: value}); 
+  }
+
   return (
     <div className="App">
-      <Form />
+      <Form values={formValues} change={handleChange}/>
     </div>
   );
 }
