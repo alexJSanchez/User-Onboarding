7579b223adfb55e8ac026a02c8f485ba03b3c465
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-31 23:03:17 -0400

    gettign returns in state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import * as yup from 'yup';
 import './App.css';
 import Form from './component/Form';
+import schema from './validation/FormSchema';
 
 const initialFormValues = {
    username: '',
@@ -8,18 +10,34 @@ const initialFormValues = {
    password: '',
    tos: false,
 }
-
-
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  tos: '',
+}
 function App() {
   const [ formValues ,  setFormValues] = useState(initialFormValues);
-  
+  const [ formErrors, setFormErrors] = useState(initialFormErrors);
+  const handleSubmit = (e) => {
+    //wip
+  }
+
+  const validate = (name, value) => {
+    yup.reach( schema, name)
+    .validate(value)
+    .then(()=> setFormErrors({...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
   const handleChange = (name, value) => {
+    validate(name,value);
     setFormValues({...formValues, [name]: value}); 
   }
 
   return (
     <div className="App">
-      <Form values={formValues} change={handleChange}/>
+      <Form values={formValues} change={handleChange} />
     </div>
   );
 }
--- src/validation/FormSchema.js
+++ src/validation/FormSchema.js
@@ -1,5 +1,22 @@
-import * from 'yup';
+import * as yup from 'yup';
 
 const formSchema = yup.object().shape({
-    
+    username: yup
+        .string()
+        .trim()
+        .required('username is required!')
+        .min(3, 'username must be more then 3 characters'),
+    email: yup
+        .string()
+        .email('must be a valid email')
+        .required('email is required'),
+    password: yup
+        .string()
+        .required('password is required')
+        .min(6, 'min 6 charecters'),
+    tos: yup
+    .boolean()
+    .oneOf([true], 'Must accept terms and conditions!'),
 })
+
+export default formSchema
