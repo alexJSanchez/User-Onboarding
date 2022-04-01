91ae2c44ed0e0f0edd7b4ff6125d3f2d0268af9d
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-31 21:01:16 -0400

    form created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Form from './component/Form'
 
 function App() {
   return (
     <div className="App">
-      
+      <Form />
     </div>
   );
 }
--- src/component/Form.js
+++ src/component/Form.js
@@ -1,8 +1,39 @@
 import React from "react";
 
 const Form = (props) => {
+    const { change , submit} = props;
+    const {username, email, password, checked} = props.values;
+   
+   const onChange = (e) => {
+    const {name, value , checked ,type} = e.target;
+    const newVal = type === 'checkbox' ? checked : value;
+    change(name,newVal);
+   }
+
+   const onSubmit = (e) =>{
+       e.preventDefault();
+       submit()
+   }
+   
     return(
-        <h1>hello</h1>
+        <div>
+            <h1>hello</h1>
+            <form onSubmit={onSubmit}>
+                <label>Name
+                    <input  type='text' name="name" value={username} onChange={onChange} />
+                </label>
+                <label>Email
+                    <input  type='email' name="email" value={email} onChange={onChange} />
+                </label>
+                <label>Password
+                    <input  type='password' name="password" value={password} onChange={onChange} />
+                </label>
+                <label>Terms of service:
+                    <input  type='checkbox' name="tos" checked={tos} onChange={onChange} />
+                </label>
+                <input type="submit" value="Create a Friend!"/>
+            </form>
+        </div>
     )
 }
 
